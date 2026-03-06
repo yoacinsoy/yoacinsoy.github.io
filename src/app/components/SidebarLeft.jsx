105b275647ed8a2b5b9d556839fdcdfbe00b9d94
yoacinsoy <brnarea@protonmail.com>
2026-03-06 00:18:51 -0300

    refactor: update class attributes to className in SidebarLeft component

--- src/app/components/SidebarLeft.jsx
+++ src/app/components/SidebarLeft.jsx
@@ -2,19 +2,19 @@ import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
 
 export default function SidebarLeft() {
     return (
-        <div id="social-icons" class="absolute top-1/2 left-2 z-20 hidden -translate-y-1/2 flex-col items-center gap-4 sm:left-6 sm:flex">
+        <div id="social-icons" className="absolute top-1/2 left-2 z-20 hidden -translate-y-1/2 flex-col items-center gap-4 sm:left-6 sm:flex">
 
-            <p class="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">HOME</a></p>
+            <p className="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">HOME</a></p>
 
-            <p class="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">ABOUT</a></p>
+            <p className="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">ABOUT</a></p>
 
-            <p class="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">CURSOS</a></p>
+            <p className="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">CURSOS</a></p>
 
-            <p class="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">CONTATO</a></p>
+            <p className="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">CONTATO</a></p>
 
-            <div class="my-2 h-16 w-px bg-gray-400 dark:bg-white/50"></div>
+            <div className="my-2 h-16 w-px bg-gray-400 dark:bg-white/50"></div>
 
-            <p class="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">TECNOLOGIAS</a></p>
+            <p className="rotate-180 text-xs font-bold tracking-widest text-blue-500 drop-shadow-[0_0_8px_#1F51FF] [writing-mode:vertical-rl]"><a href="#home">TECNOLOGIAS</a></p>
         </div>
     )
 }
