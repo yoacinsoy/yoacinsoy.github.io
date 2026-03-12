275ac2d4fcd720745ab6e52e2e7fde0afd8ef3be
yoacinsoy <brnarea@protonmail.com>
2026-03-12 17:35:13 -0300

    feat: remove Works component and update project structure
    Removed the Works.jsx component from the portfolio project and updated the project structure by removing the associated component file. This change simplifies the codebase by eliminating the project showcase section.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Header from "@/app/components/layout/Header";
 import Grid from "@/app/components/ui/Grid";
 import ParticlesBackground from "./components/ui/ParticlesBackground";
 import Welcome from "@/app/components/sections/Welcome";
-import Works from "@/app/components/sections/Works";
+import Projects from "@/app/components/sections/Projects";
 
 export default function Home() {
     return (
@@ -11,7 +11,7 @@ export default function Home() {
             <Grid />
             <ParticlesBackground />
             <Welcome />
-            <Works />
+            <Projects />
         </div>
     );
 }
